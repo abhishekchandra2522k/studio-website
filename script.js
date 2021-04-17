18136616c8ed07fb679b76e0b6a30b79ed505aa2
abhishekchandra2522k <1ac23456789@gmail.com>
2021-04-17 22:13:46 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function(event){
+
+    const engagement = document.getElementById("serviceOne");
+    const serviceBox = document.getElementsByClassName("serviceBox");
+    const toggleMenu = document.getElementById("toggleMenu");
+    const collMenu = document.getElementById("collMenu");
+    
+    console.log("loaded");
+
+    window.addEventListener('scroll', function(){
+        var navi = document.querySelector('.header');
+
+        if(this.window.scrollY > 0) {
+            navi.className += ' scrolledHeader';
+        } else {
+            console.log("reached top");
+            navi.classList.remove("scrolledHeader");
+        }
+    });
+
+    for(i = 0; i < serviceBox.length; i++) {
+        serviceBox[i].addEventListener('touchstart', function(){
+            this.classname= '.hover';
+            console.log('touchstart');
+        });
+    }
+
+    toggleMenu.addEventListener('change', function(event){
+        if(toggleMenu.checked) {
+            collMenu.style.display = "block";
+            console.log("checked");
+        } else {
+            collMenu.style.display = "none";
+            console.log("unchecked");
+        }
+    });
+        
+
+});
